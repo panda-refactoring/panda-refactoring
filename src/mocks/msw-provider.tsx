3573f2ce2c6248154c41007315ad898b0ec62553
joeun-lee <Joeun.lee@switchwon.com>
2024-07-15 18:24:11 +0900

    msw 404에러해결

--- src/mocks/msw-provider.tsx
+++ src/mocks/msw-provider.tsx
@@ -1,20 +1,18 @@
 import { PropsWithChildren, useEffect, useState } from 'react';
 import { initMSW } from '.';
 
-const isAPIMockingMode = process.env.NEXT_PUBLIC_API_MOCKING === 'enabled';
-
 const MSWProvider = ({ children }: PropsWithChildren) => {
   const [isReady, setIsReady] = useState(false);
-
+  
   useEffect(() => {
-    const init = async () => {
-      if (isAPIMockingMode) {
-        await initMSW();
-        setIsReady(true);
-      }
-    };
+    const enableMocking = async () => {
+      if (process.env.NEXT_PUBLIC_API_MOCKING !== 'enabled') return;
+    
+      await initMSW();
+      setIsReady(true);
+    }
 
-    if (!isReady) init();
+    if (!isReady) enableMocking();
   }, [isReady]);
 
   if (!isReady) return null;
